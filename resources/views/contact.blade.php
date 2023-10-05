c0690339e69bea5fdedada0969fc0620e8a38654
Rakapurbayu <rakapurbayu.tito.aryasaputra-2022@vokasi.unair.ac.id>
2023-10-05 21:41:50 +0700

    kontak kami

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -30,7 +30,7 @@
     <link rel="stylesheet" href="css/style.css" />
 
     <title>
-      Property &mdash; Free Bootstrap 5 Website Template by Untree.co
+      Kami TitikKumpul &mdash;
     </title>
   </head>
   <body>
@@ -47,19 +47,18 @@
       <div class="container">
         <div class="menu-bg-wrap">
           <div class="site-navigation">
-            <a href="index.html" class="logo m-0 float-start">Property</a>
+            <a href="index.html" class="logo m-0 float-start">Kami TitikKumpul</a>
 
             <ul
               class="js-clone-nav d-none d-lg-inline-block text-start site-menu float-end"
             >
               <li><a href="index.html">Home</a></li>
               <li class="has-children">
-                <a href="properties.html">Properties</a>
+                <a href="properties.blade.php">Pesan Tempat Nongkrong</a>
                 <ul class="dropdown">
-                  <li><a href="#">Buy Property</a></li>
-                  <li><a href="#">Sell Property</a></li>
+                  <li><a href="#">Pesan Menu</a></li>
+                  <li><a href="#">Pesan Tempat</a></li>
                   <li class="has-children">
-                    <a href="#">Dropdown</a>
                     <ul class="dropdown">
                       <li><a href="#">Sub Menu One</a></li>
                       <li><a href="#">Sub Menu Two</a></li>
@@ -68,9 +67,8 @@
                   </li>
                 </ul>
               </li>
-              <li><a href="services.html">Services</a></li>
-              <li><a href="about.html">About</a></li>
-              <li class="active"><a href="contact.html">Contact Us</a></li>
+              <li><a href="about.blade.php">Tentang Kami</a></li>
+              <li class="active"><a href="contact.blade.php">Kontak Kami</a></li>
             </ul>
 
             <a
@@ -93,7 +91,7 @@
       <div class="container">
         <div class="row justify-content-center align-items-center">
           <div class="col-lg-9 text-center mt-5">
-            <h1 class="heading" data-aos="fade-up">Contact Us</h1>
+            <h1 class="heading" data-aos="fade-up">Hubungi Kami</h1>
 
             <nav
               aria-label="breadcrumb"
@@ -101,12 +99,12 @@
               data-aos-delay="200"
             >
               <ol class="breadcrumb text-center justify-content-center">
-                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                <li class="breadcrumb-item"><a href="index.blade.php">Home</a></li>
                 <li
                   class="breadcrumb-item active text-white-50"
                   aria-current="page"
                 >
-                  Contact
+                  Kontak Kami
                 </li>
               </ol>
             </nav>
@@ -126,32 +124,32 @@
             <div class="contact-info">
               <div class="address mt-2">
                 <i class="icon-room"></i>
-                <h4 class="mb-2">Location:</h4>
+                <h4 class="mb-2">Lokasi Kami Berada:</h4>
                 <p>
-                  43 Raymouth Rd. Baltemoer,<br />
-                  London 3910
+                  Jalan Sutoreja Tengah VI 12<br />
+                  Surabaya 16123
                 </p>
               </div>
 
               <div class="open-hours mt-4">
                 <i class="icon-clock-o"></i>
-                <h4 class="mb-2">Open Hours:</h4>
+                <h4 class="mb-2">Buka Pada:</h4>
                 <p>
-                  Sunday-Friday:<br />
-                  11:00 AM - 2300 PM
+                  Senin-Jum'at:<br />
+                  08:00 - 16.00
                 </p>
               </div>
 
               <div class="email mt-4">
                 <i class="icon-envelope"></i>
                 <h4 class="mb-2">Email:</h4>
-                <p>info@Untree.co</p>
+                <p>titikkumpul@mari.co.id</p>
               </div>
 
               <div class="phone mt-4">
                 <i class="icon-phone"></i>
-                <h4 class="mb-2">Call:</h4>
-                <p>+1 1234 55488 55</p>
+                <h4 class="mb-2">Kontak:</h4>
+                <p>Telephone: +62 434 2210 3910</p>
               </div>
             </div>
           </div>
@@ -162,21 +160,21 @@
                   <input
                     type="text"
                     class="form-control"
-                    placeholder="Your Name"
+                    placeholder="Nama"
                   />
                 </div>
                 <div class="col-6 mb-3">
                   <input
                     type="email"
                     class="form-control"
-                    placeholder="Your Email"
+                    placeholder="Email"
                   />
                 </div>
                 <div class="col-12 mb-3">
                   <input
                     type="text"
                     class="form-control"
-                    placeholder="Subject"
+                    placeholder="Keterangan"
                   />
                 </div>
                 <div class="col-12 mb-3">
@@ -186,14 +184,14 @@
                     cols="30"
                     rows="7"
                     class="form-control"
-                    placeholder="Message"
+                    placeholder="Pesan anda untuk kami"
                   ></textarea>
                 </div>
 
                 <div class="col-12">
                   <input
                     type="submit"
-                    value="Send Message"
+                    value="Kirim Pesan"
                     class="btn btn-primary"
                   />
                 </div>
@@ -210,13 +208,12 @@
         <div class="row">
           <div class="col-lg-4">
             <div class="widget">
-              <h3>Contact</h3>
-              <address>43 Raymouth Rd. Baltemoer, London 3910</address>
+              <h3>Kontak</h3>
+              <address> Jalan Sutoreja Tengah VI 12 Surabaya 16123</address>
               <ul class="list-unstyled links">
-                <li><a href="tel://11234567890">+1(123)-456-7890</a></li>
-                <li><a href="tel://11234567890">+1(123)-456-7890</a></li>
+                <li><a href="tel://11234567890">+62 434 2210 3910</a></li>
                 <li>
-                  <a href="mailto:info@mydomain.com">info@mydomain.com</a>
+                  <a href="mailto:info@mydomain.com">titikkumpul@mari.co.id</a>
                 </li>
               </ul>
             </div>
@@ -225,35 +222,23 @@
           <!-- /.col-lg-4 -->
           <div class="col-lg-4">
             <div class="widget">
-              <h3>Sources</h3>
+              <h3>Sumber</h3>
               <ul class="list-unstyled float-start links">
-                <li><a href="#">About us</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Vision</a></li>
-                <li><a href="#">Mission</a></li>
-                <li><a href="#">Terms</a></li>
-                <li><a href="#">Privacy</a></li>
+                <li><a href="#">Tentang Kami</a></li>
+                <li><a href="#">Pemesanan</a></li>
+                <li><a href="#">Visi & Misi</a></li>
+                <li><a href="#">Terms & Privacy</a></li>
+                <li><a href="#">Partner</a></li>
               </ul>
               <ul class="list-unstyled float-start links">
-                <li><a href="#">Partners</a></li>
-                <li><a href="#">Business</a></li>
-                <li><a href="#">Careers</a></li>
-                <li><a href="#">Blog</a></li>
+                <li><a href="#">Busines</a></li>
+                <li><a href="#">Karir</a></li>
                 <li><a href="#">FAQ</a></li>
-                <li><a href="#">Creative</a></li>
+                <li><a href="#">Kontak</a></li>
               </ul>
             </div>
             <!-- /.widget -->
           </div>
-          <!-- /.col-lg-4 -->
-          <div class="col-lg-4">
-            <div class="widget">
-              <h3>Links</h3>
-              <ul class="list-unstyled links">
-                <li><a href="#">Our Vision</a></li>
-                <li><a href="#">About us</a></li>
-                <li><a href="#">Contact us</a></li>
-              </ul>
 
               <ul class="list-unstyled social">
                 <li>
@@ -267,12 +252,6 @@
                 </li>
                 <li>
                   <a href="#"><span class="icon-linkedin"></span></a>
-                </li>
-                <li>
-                  <a href="#"><span class="icon-pinterest"></span></a>
-                </li>
-                <li>
-                  <a href="#"><span class="icon-dribbble"></span></a>
                 </li>
               </ul>
             </div>
