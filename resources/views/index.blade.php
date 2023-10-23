dcbac15b37ade64e7f127102b057af499a93b745
FerryCalvin <110297689+FerryCalvin@users.noreply.github.com>
2023-10-23 19:12:35 +0700

    Route Fix
    Senin, 23/10/2023 19:13 PM
    Rumah Kopi Baik

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,14 +4,100 @@
 * Template URI: https://untree.co/
 * License: https://creativecommons.org/licenses/by/3.0/
 */ -->
-@extends('header')
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="author" content="Untree.co" />
+    <link rel="shortcut icon" href="favicon.png" />
 
-@section('title', 'TitikKumpul — Beranda')
+    <meta name="description" content="" />
+    <meta name="keywords" content="bootstrap, bootstrap5" />
 
-@section('content')
-<div class="hero">
-    <!-- Hero section content -->
-</div>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700&display=swap"
+      rel="stylesheet"
+    />
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css" />
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css" />
+
+    <link rel="stylesheet" href="css/tiny-slider.css" />
+    <link rel="stylesheet" href="css/aos.css" />
+    <link rel="stylesheet" href="css/style.css" />
+
+    <title>
+      TitikKumpul &mdash;
+    </title>
+  </head>
+  <body>
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close">
+          <span class="icofont-close js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+
+    <nav class="site-nav">
+      <div class="container">
+        <div class="menu-bg-wrap">
+          <div class="site-navigation">
+            <a href="index.html" class="logo m-0 float-start">TitikKumpul</a>
+
+            <ul
+              class="js-clone-nav d-none d-lg-inline-block text-start site-menu float-end"
+            >
+              <li class="active"><a href="{{ route('home') }}">Home</a></li>
+              <li class="has-children">
+                <a href="{{ route('PesanNongkrong') }}">PesanNongkrong</a>
+                <ul class="dropdown">
+                  <li><a href="#">Pesan Tempat</a></li>
+                  <li><a href="#">Pesan Makan</a></li>
+                    <ul class="dropdown">
+                      <li><a href="#">Sub Menu One</a></li>
+                      <li><a href="#">Sub Menu Two</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <li><a href="{{ route('TentangKami') }}">Tentang Kami</a></li>
+              <li><a href="{{ route('KontakKami') }}">Kontak kami</a></li>
+            </ul>
+
+            <a
+              href="#"
+              class="burger light me-auto float-end mt-1 site-menu-toggle js-menu-toggle d-inline-block d-lg-none"
+              data-toggle="collapse"
+              data-target="#main-navbar"
+            >
+              <span></span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <div class="hero">
+      <div class="hero-slide">
+        <div
+          class="img overlay"
+          style="background-image: url('images/hero_bg_3.jpg')"
+        ></div>
+        <div
+          class="img overlay"
+          style="background-image: url('images/hero_bg_2.jpg')"
+        ></div>
+        <div
+          class="img overlay"
+          style="background-image: url('images/hero_bg_1.jpg')"
+        ></div>
+      </div>
+
       <div class="container">
         <div class="row justify-content-center align-items-center">
           <div class="col-lg-9 text-center">
@@ -57,9 +143,7 @@
                 class="btn btn-primary text-white py-3 px-4"
                 >Lihat Pilihan TitikKumpul</a
               >
-
             </p>
-
           </div>
         </div>
         <div class="row">
@@ -72,13 +156,12 @@
                   </a>
 
                   <div class="property-content">
-                    <div class="price mb-2"><span>The Golden Goose Restaurant</span></div>
+                    <div class="price mb-2"><span>$1,291,000</span></div>
                     <div>
                       <span class="d-block mb-2 text-black-50"
-                        >Jalan Sisingamangaraja No. 10
-</span
+                        >5232 California Fake, Ave. 21BC</span
                       >
-                      <span class="city d-block mb-3">Denpasar, Bali</span>
+                      <span class="city d-block mb-3">California, USA</span>
 
                       <div class="specs d-flex mb-4 py-0">
                       <a
@@ -104,14 +187,12 @@
                   </a>
 
                   <div class="property-content">
-                    <div class="price mb-2"><span>The Blue Plate Restaurant
-</span></div>
+                    <div class="price mb-2"><span>$1,291,000</span></div>
                     <div>
                       <span class="d-block mb-2 text-black-50"
-                        >Jalan Kapten Pattimura No. 88
-</span
+                        >5232 California Fake, Ave. 21BC</span
                       >
-                      <span class="city d-block mb-3">Manado, Sulawesi Utara</span>
+                      <span class="city d-block mb-3">California, USA</span>
 
                       <div class="specs d-flex mb-4">
                       <a
@@ -137,13 +218,12 @@
                   </a>
 
                   <div class="property-content">
-                    <div class="price mb-2"><span>The Red Lion Restaurant</span></div>
+                    <div class="price mb-2"><span>$1,291,000</span></div>
                     <div>
                       <span class="d-block mb-2 text-black-50"
-                        >Jalan Ahmad Yani No. 789
-</span
+                        >5232 California Fake, Ave. 21BC</span
                       >
-                      <span class="city d-block mb-3">Makassar, Sulawesi Selatan</span>
+                      <span class="city d-block mb-3">California, USA</span>
 
                       <div class="specs d-flex mb-4">
                       <a
@@ -169,13 +249,12 @@
                   </a>
 
                   <div class="property-content">
-                    <div class="price mb-2"><span> Oby's Restaurant</span></div>
+                    <div class="price mb-2"><span>$1,291,000</span></div>
                     <div>
                       <span class="d-block mb-2 text-black-50"
-                        >Jalan Gatot Subroto No. 234
-</span
+                        >5232 California Fake, Ave. 21BC</span
                       >
-                      <span class="city d-block mb-3">Bandung, Jawa Barat</span>
+                      <span class="city d-block mb-3">California, USA</span>
 
                       <div class="specs d-flex mb-4">
                       <a
@@ -195,7 +274,159 @@
                 </div>
                 <!-- .item -->
 
+                <div class="property-item">
+                  <a href="property-single.html" class="img">
+                    <img src="images/img_5.jpg" alt="Image" class="img-fluid" />
+                  </a>
 
+                  <div class="property-content">
+                    <div class="price mb-2"><span>$1,291,000</span></div>
+                    <div>
+                      <span class="d-block mb-2 text-black-50"
+                        >5232 California Fake, Ave. 21BC</span
+                      >
+                      <span class="city d-block mb-3">California, USA</span>
+
+                      <div class="specs d-flex mb-4">
+                      <a
+                        href="property-single.html"
+                        class="btn btn-primary py-2 px-3"
+                        >See Menu</a
+                      >
+                      </div>
+
+                      <a
+                        href="property-single.html"
+                        class="btn btn-primary py-2 px-3"
+                        >See details</a
+                      >
+                    </div>
+                  </div>
+                </div>
+                <!-- .item -->
+
+                <div class="property-item">
+                  <a href="property-single.html" class="img">
+                    <img src="images/img_6.jpg" alt="Image" class="img-fluid" />
+                  </a>
+
+                  <div class="property-content">
+                    <div class="price mb-2"><span>$1,291,000</span></div>
+                    <div>
+                      <span class="d-block mb-2 text-black-50"
+                        >5232 California Fake, Ave. 21BC</span
+                      >
+                      <span class="city d-block mb-3">California, USA</span>
+
+                      <div class="specs d-flex mb-4">
+                      <a
+                        href="property-single.html"
+                        class="btn btn-primary py-2 px-3"
+                        >See Menu</a
+                      >
+                      </div>
+
+                      <a
+                        href="property-single.html"
+                        class="btn btn-primary py-2 px-3"
+                        >See details</a
+                      >
+                    </div>
+                  </div>
+                </div>
+                <!-- .item -->
+
+                <div class="property-item">
+                  <a href="property-single.html" class="img">
+                    <img src="images/img_7.jpg" alt="Image" class="img-fluid" />
+                  </a>
+
+                  <div class="property-content">
+                    <div class="price mb-2"><span>$1,291,000</span></div>
+                    <div>
+                      <span class="d-block mb-2 text-black-50"
+                        >5232 California Fake, Ave. 21BC</span
+                      >
+                      <span class="city d-block mb-3">California, USA</span>
+
+                      <div class="specs d-flex mb-4">
+                      <a
+                        href="property-single.html"
+                        class="btn btn-primary py-2 px-3"
+                        >See Menu</a
+                      >
+                      </div>
+
+                      <a
+                        href="property-single.html"
+                        class="btn btn-primary py-2 px-3"
+                        >See details</a
+                      >
+                    </div>
+                  </div>
+                </div>
+                <!-- .item -->
+
+                <div class="property-item">
+                  <a href="property-single.html" class="img">
+                    <img src="images/img_8.jpg" alt="Image" class="img-fluid" />
+                  </a>
+
+                  <div class="property-content">
+                    <div class="price mb-2"><span>$1,291,000</span></div>
+                    <div>
+                      <span class="d-block mb-2 text-black-50"
+                        >5232 California Fake, Ave. 21BC</span
+                      >
+                      <span class="city d-block mb-3">California, USA</span>
+
+                      <div class="specs d-flex mb-4">
+                      <a
+                        href="property-single.html"
+                        class="btn btn-primary py-2 px-3"
+                        >See Menu</a
+                      >
+                      </div>
+
+                      <a
+                        href="property-single.html"
+                        class="btn btn-primary py-2 px-3"
+                        >See details</a
+                      >
+                    </div>
+                  </div>
+                </div>
+                <!-- .item -->
+
+                <div class="property-item">
+                  <a href="property-single.html" class="img">
+                    <img src="images/img_1.jpg" alt="Image" class="img-fluid" />
+                  </a>
+
+                  <div class="property-content">
+                    <div class="price mb-2"><span>$1,291,000</span></div>
+                    <div>
+                      <span class="d-block mb-2 text-black-50"
+                        >5232 California Fake, Ave. 21BC</span
+                      >
+                      <span class="city d-block mb-3">California, USA</span>
+
+                      <div class="specs d-flex mb-4">
+                      <a
+                        href="property-single.html"
+                        class="btn btn-primary py-2 px-3"
+                        >See Menu</a
+                      >
+                      </div>
+
+                      <a
+                        href="property-single.html"
+                        class="btn btn-primary py-2 px-3"
+                        >See details</a
+                      >
+                    </div>
+                  </div>
+                </div>
                 <!-- .item -->
               </div>
 
@@ -527,13 +758,13 @@
     <div class="section">
       <div class="row justify-content-center footer-cta" data-aos="fade-up">
         <div class="col-lg-7 mx-auto text-center">
-          <h2 class="mb-4">Mari bergabung menjadi salah satu TitikKumpul!</h2>
+          <h2 class="mb-4">Be a part of our growing real state agents</h2>
           <p>
             <a
               href="#"
               target="_blank"
               class="btn btn-primary text-white py-3 px-4"
-              >Bergabung dengan TitikKumpul</a
+              >Apply for Real Estate agent</a
             >
           </p>
         </div>
@@ -792,7 +1023,6 @@
 
         <script src="js/bootstrap-datetimepicker.min.js"></script>
 <script></script>
-@endsection
 
   </body>
 </html>
