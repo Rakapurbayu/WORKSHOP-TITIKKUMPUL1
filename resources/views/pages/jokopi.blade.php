24e9622a585b27128f7438f152a043ccde44c10a
omsentod <rifqiwidyadana6@gmail.com>
2023-11-05 22:48:38 +0700

    jokopi new view
    lebih proper

--- resources/views/pages/jokopi.blade.php
+++ resources/views/pages/jokopi.blade.php
@@ -10,23 +10,146 @@
   <meta name="theme-color" content="#000000" />
   <title>Desktop - 1</title>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter%3A400"/>
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro%3A400"/>
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Katibeh%3A400"/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro%3A400%2C700"/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit%3A400"/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kadwa%3A400%2C700"/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inria+Sans%3A400"/>
   <link rel="stylesheet" href="./styles/desktop-1.css"/>
 </head>
 <body>
-<div class="desktop-1-Tov">
-  <div class="image-1-oFW">
+<div class="desktop-1-7eh">
+  <div class="auto-group-fnnf-JDP">
+    <div class="image-1-CCH">
+    </div>
+    <div class="restaurantcard-details-eiV">
+    </div>
+    <div class="group-1-uPX">
+      <div class="auto-group-jpa7-1xM">
+        <div class="auto-group-acep-vZX">
+          <p class="overview-Eq7">Overview</p>
+          <p class="menu-eXT">Menu</p>
+          <p class="reviews-Ycq">Reviews</p>
+        </div>
+        <div class="auto-group-ltf1-HKX">
+          <img class="chatbubble-zzd" src="./assets/chatbubble.png"/>
+          <p class="no-reviews-WCH">No reviews</p>
+          <img class="monetizationon-Pms" src="./assets/monetizationon.png"/>
+          <p class="k-30k-iJM">15K-30K</p>
+          <img class="localdining-RTf" src="./assets/localdining.png"/>
+          <p class="international-Ms7">International</p>
+        </div>
+        <p class="jokopi-cafe-surabaya-Gz5">
+        JOKOPI - Cafe 
+        <br/>
+        Surabaya
+        </p>
+        <p class="photos-ydb">4 Photos</p>
+        <div class="auto-group-dtat-5wX">
+          <p class="menu-atH">Menu</p>
+          <p class="be-the-first-to-review-this-restaurant-uff">Be the first to review this restaurant</p>
+          <p class="overall-rating-reviews-C8y">Overall rating reviews</p>
+          <p class="reviews-can-only-be-made-by-diners-who-have-eaten-at-this-restaurant-UMP">
+          Reviews can only be made by diners who have eaten at this restaurant
+          <br/>
+          
+          </p>
+          <img class="line-1-wEy" src="./assets/line-1-Rpd.png"/>
+          <p class="view-menu-on-restaurants-website-4Kb">View menu on restaurant&#39;s website</p>
+          <img class="line-1-ub7" src="./assets/line-1-Pgu.png"/>
+          <img class="captiveportal-dn1" src="./assets/captiveportal.png"/>
+        </div>
+        <div class="auto-group-puvz-kLq">
+          <div class="stars-UXj">
+            <img class="starratehalf-p5o" src="./assets/starratehalf-v7B.png"/>
+            <img class="starratehalf-MbX" src="./assets/starratehalf-Q3B.png"/>
+            <img class="starratehalf-63K" src="./assets/starratehalf.png"/>
+            <img class="starratehalf-EQR" src="./assets/starratehalf-T13.png"/>
+            <img class="starratehalf-BKf" src="./assets/starratehalf-HDB.png"/>
+          </div>
+          <p class="based-on-ratings-8Eu">5 Based on ratings</p>
+        </div>
+        <img class="line-1-variant2-rgh" src="./assets/line-1-variant2-2n5.png"/>
+        <img class="line-1-variant2-CEm" src="./assets/line-1-variant2.png"/>
+        <div class="auto-group-y5rv-Xnq">
+          <img class="snapinsta-1-fPF" src="./assets/snapinsta-1.png"/>
+          <img class="snapinsta-zwK" src="./assets/snapinsta-tGR.png"/>
+        </div>
+        <div class="auto-group-vkqd-LVP">
+          <img class="snapinsta-5C5" src="./assets/snapinsta.png"/>
+          <div class="auto-group-apqv-DJH">
+            <img class="snapinsta-mah" src="./assets/snapinsta-gDF.png"/>
+            <img class="grade-iF3" src="./assets/grade.png"/>
+          </div>
+        </div>
+        <p class="for-reservation-of-10-guests-and-more-please-contact-62812-3351-8156-or-62-8123-454-6298get-fancy-in-luxury-dining-at-the-award-winning-restaurant-magnolia-overlooking-the-cityscape-of-surabaya-the-restaurant-serves-international-favorites-cuisines-such-as-meat-carvings-and-ocean-fresh-seafood-and-curated-health-and-well-being-in-mind-with-its-refined-urban-vibe-and-open-plan-kitchen-this-venue-celebrates-the-universally-cherished-pleasures-of-good-times-and-occasions-2Wd">For Reservation of 10 Guests and More, please contact: +62812 3351 8156 or +62 8123 454 6298Get fancy in luxury dining at the award-winning restaurant. Magnolia, Overlooking the cityscape of Surabaya, the restaurant serves International favorites cuisines such as meat carvings and ocean-fresh seafood and curated health and well-being in mind. With its refined urban vibe and open-plan kitchen, this venue celebrates the universally cherished pleasures of good times and occasions.</p>
+        <div class="auto-group-78kd-w1K">
+          <div class="auto-group-cdlf-F21">
+            <div class="auto-group-j9ek-N6d">
+              <div class="services-checklistpoint-usF">
+                <img class="checklist-EuX" src="./assets/checklist-TXo.png"/>
+                <p class="cleaning-sanitizing-YvD"> Cleaning &amp; sanitizing</p>
+              </div>
+              <div class="services-checklistpoint-TXP">
+                <img class="checklist-o5T" src="./assets/checklist-gV7.png"/>
+                <p class="screening-vfs">Screening</p>
+              </div>
+            </div>
+            <div class="services-checklistpoint-GDw">
+              <img class="checklist-RMj" src="./assets/checklist.png"/>
+              <p class="sick-staff-prohibited-in-workplace-9oX">
+              Sick staff prohibited in 
+              <br/>
+              workplace
+              </p>
+            </div>
+          </div>
+          <div class="auto-group-bxsf-sDj">
+            <div class="services-checklistpoint-ot5">
+              <img class="checklist-xFB" src="./assets/checklist-7p5.png"/>
+              <p class="contacless-payment-available-Gmf">Contacless payment available</p>
+            </div>
+            <div class="services-checklistpoint-PrH">
+              <img class="checklist-9Kf" src="./assets/checklist-3Kf.png"/>
+              <p class="commond-areas-deep-cleaned-daily-Usj">Commond areas deep-cleaned daily</p>
+            </div>
+            <div class="services-checklistpoint-D4d">
+              <img class="checklist-AEm" src="./assets/checklist-kKb.png"/>
+              <p class="staff-vaccinated-W3j"> Staff Vaccinated</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="auto-group-gzck-yxu">
+        <div class="auto-group-g3tr-6Xj">
+          <p class="make-reservations-pCq">Make Reservations</p>
+          <img class="line-1-variant2-x49" src="./assets/line-1-variant2-bCd.png"/>
+        </div>
+        <div class="auto-group-ppx3-tTb">
+          <p class="party-size-3LV">Party size</p>
+          <p class="people-AR7">2 People</p>
+          <div class="line-2-saR">
+          </div>
+        </div>
+        <div class="auto-group-ryt3-C6u">
+          <div class="auto-group-kew5-Lyo">
+            <p class="date-Vbo">Date</p>
+            <p class="nov-52023-pe5">Nov 5,2023</p>
+            <img class="line-3-k1w" src="REPLACE_IMAGE:14:172"/>
+            <img class="arrowforwardios-5pu" src="./assets/arrowforwardios.png"/>
+          </div>
+          <div class="auto-group-mpns-DAR">
+            <p class="time-xdo">Time</p>
+            <p class="pm-V7w">9:30 PM</p>
+            <img class="line-4-nsj" src="REPLACE_IMAGE:14:178"/>
+          </div>
+        </div>
+      </div>
+      <img class="line-1-variant2-L8Z" src="REPLACE_IMAGE:4:5"/>
+    </div>
+    <div class="find-button-sPP">Reservasi</div>
+    <img class="arrowforwardios-8q7" src="./assets/arrowforwardios-4bK.png"/>
+    <img class="arrowforwardios-TsP" src="./assets/arrowforwardios-Wc9.png"/>
   </div>
-  <div class="rectangle-1-NC8">
-  </div>
-  <p class="overview-MyA">Overview</p>
-  <p class="menu-Cit">Menu</p>
-  <p class="reviews-hQk">Reviews</p>
-  <p class="jokopi-cafe-surabaya-c1v">JOKOPI - Cafe Surabaya</p>
-  <img class="line-1-variant2-fVz" src="REPLACE_IMAGE:4:5"/>
-  <img class="line-1-variant2-PB6" src="./assets/line-1-variant2-vac.png"/>
-  <img class="line-1-variant2-ufE" src="./assets/line-1-variant2.png"/>
 </div>
 </body>
 @endsection
